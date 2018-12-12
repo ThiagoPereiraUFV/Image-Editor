da85864ff96fbf7f8a4dbd36bb2c5f6268a61cec
ThiagoPereiraUFV <thiago.marinho@ufv.br>
2018-12-12 16:54:06 -0200

    Update RGB.h

--- RGB.h
+++ RGB.h
@@ -8,6 +8,8 @@ class RGB{
     public:
         RGB(const int R, const int G, const int B) : R(R), G(G), B(B) {};
         RGB(const RGB &obj) {   *this = obj;    }
+        RGB() : R(0), G(0), B(0) {};
+        ~RGB() {};
         RGB &operator=(const RGB &obj) {
             if(this == &obj)
                 return *this;
@@ -57,19 +59,19 @@ class RGB{
 
             return *this;
         }
-        RGB operator*(const int &obj) {
+        RGB operator*(const int &obj) const{
             RGB aux(0, 0, 0);
 
-            aux.R = this->R*obj;
-            aux.G = this->G*obj;
-            aux.B = this->B*obj;
+            aux.R = (this->R)*(obj);
+            aux.G = (this->G)*(obj);
+            aux.B = (this->B)*(obj);
 
             return aux;
         }
-        bool operator>(const int &obj) {
+        bool operator>(const int &obj) const{
             return ((this->R > obj) || (this->G > obj) || (this->B > obj));
         }
-        bool operator<(const int &obj) {
+        bool operator<(const int &obj) const{
             return ((this->R < obj) || (this->G < obj) || (this->B < obj));
         }
         friend ostream &operator<<(ostream &stream, const RGB &obj) {
