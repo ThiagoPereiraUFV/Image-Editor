43e930b6749b8647c428a87c74da269f662174d2
ThiagoPereiraUFV <thiago.marinho@ufv.br>
2019-04-11 14:35:18 -0300

    Update imageEditor.cpp

--- imageEditor.cpp
+++ imageEditor.cpp
@@ -4,27 +4,16 @@
 #include <vector>
 #include <numeric>
 #include <cmath>
+#include <unistd.h>
 #include "RGB.h"
 #include "functions.cpp"
+#include "interface.h"
 
 using namespace std;
 
 int main() {
-    bool read;
-    int maxPixels;
-    string type;
-    vector<vector<int> > imageP2;
-    vector<vector<RGB> > imageP3, f1, f2;
-
-    read = readFile(imageP2, imageP3, maxPixels, type);
-    f1.resize(imageP3.size(), vector<RGB> (imageP3[0].size()));
-    f2 = f1;
-
-    maskP3(f1, imageP3, type, chooseFilter(0));
-    maskP3(f2, imageP3, type, chooseFilter(1));
-
-    imageP3 = unify(f1, f2);
-    print(imageP2, imageP3, maxPixels, type, read);
-
+    Menu m;
+    m.selOption();
+    
     return 0;
 }
